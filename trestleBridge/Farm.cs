de77dc39769b640b4f6011ec8c9440f6cd141648
VERNON WESTMORELAND <vfwestmoreland@gmail.com>
2022-08-06 11:30:17 -0500

    Test

--- trestleBridge/Farm.cs
+++ trestleBridge/Farm.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using trestleBridge;
 
-namespace trestleBridge
+namespace trestleBridge.Models.Facilities
 {
     public class Farm
     {
